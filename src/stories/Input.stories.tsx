0c5da0b582c3b3d97171198fce38b1afec16d549
Aleksandr_UB <bogdanov777000@mail.ru>
2022-07-08 20:27:00 +0300

    input GetValueOfUncontrolledInputByPressButton

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,74 +1,45 @@
-// storybook - это стенд компонент...
-import React, {ChangeEvent, useRef, useState} from 'react';
-import {action} from '@storybook/addon-actions';
-// настройки:
+import React, {useRef, useState} from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {Button} from './Button';
+
 export default {
-    title: 'input',
-    // component: input,
+    title: 'input_cat',
+    // component: Input,
 }
 
 export const UncontrolledInput = () => <input/>
-export const TrackValueUncontrolledInput = () => {
+export const ControlledInput = () => <input value="It-cat-incubator"/>
+// наблюдаем за изменением значения input в дочке:
+export const TrackUncontrolledInput = () => {
     const [value, setValue] = useState('')
-    // у некотролируемого инпута можем прокинуть колбек и на изменения получать инфу что поменялось:
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const actualValue = event.currentTarget.value
-        setValue(actualValue)
+
+    return (
+        <> <input
+            onChange={(e) => {
+                e.currentTarget.value;
+                setValue(e.currentTarget.value)
+            }}
+            value={value}/> ---- {value}
+        </>
+    )
+}
+export const GetValueOfUncontrolledInputByPressButton = () => {
+    const [value, setValue] = useState<>('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const el = inputRef.current as HTMLInputElement;
+        setValue(el.value)
     }
 
     return <>
-        <input onChange={onChange}/>
-        --- {value} ---
+        (<input ref={inputRef}/>
+        <button
+            onClick={save}
+        >save
+        </button>
+        --- actual value: {value}
+        )
     </>
 }
-export const GetValueOfUncontrolledInputByButtonPress = () => {
-    const [value, setValue] = useState('')
-    // ref позволяет нам создать ссылку на элемент и связать ее с ним и в коде обращаться к ней по каррент...
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    const save = () => {
-        const inputEl = inputRef.current as HTMLInputElement
-        setValue(inputEl.value)
-    }
-
-    return <><input ref={inputRef} id={'inputId'}/><button onClick={save}>sav</button>actual value: {value}</>
-}
-
-export const ControlledInput = () => {
-    const [parentValue,setParentValue] = useState("")
-    // action выплевывает колбек - что же прило в наш колбек какие значения - превращается в колбек с бесконечным количеством аргументов:
-   // return <input value={parentValue} onChange={action("Want to change")}/>
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setParentValue(e.currentTarget.value)
-    }
-    return <input value={parentValue} onChange={ onChange }/>
-}
-
-export const ControlledCheckbox = () => {
-    const [parentValue,setParentValue] = useState(true)
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setParentValue(e.currentTarget.checked)
-    }
-    return <input type={"checkbox"} checked={parentValue} onChange={ onChange }/>
-}
-
-export const ControlledSelect = () => {
-    // const [parentValue, setParentValue] = useState<string | undefined>(undefined)
-    const [parentValue, setParentValue] = useState<string | undefined>("3") /*захардкодили значение на Kiev*/
-    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setParentValue(e.currentTarget.value) /*берем значение*/
-    }
-    // select по умолчанию неконтролируемый
-        return <select value={parentValue} onChange={onChange}>
-            {/*у опций значение как правило id*/}
-            <option>none</option>
-            <option value={"1"}>Minsk</option>
-            <option value={"2"}>Moscow</option>
-            <option value={"3"}>Kiev</option>
-        </select>
 
 
-
-
-    // export const ControlledInputFixedValue = () => <input value="It-incubator"/>
-}
