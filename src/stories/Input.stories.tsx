67bff759c36b4111a9a7a7d24f2b275c77307f8b
Aleksandr_UB <bogdanov777000@mail.ru>
2022-07-08 20:33:21 +0300

    input GetValueOfUncontrolledInputByPressButton UncontrolledInput ControlledInput

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 import {Button} from './Button';
 
@@ -12,34 +12,29 @@ export const ControlledInput = () => <input value="It-cat-incubator"/>
 // наблюдаем за изменением значения input в дочке:
 export const TrackUncontrolledInput = () => {
     const [value, setValue] = useState('')
+    const onInputChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+        e.currentTarget.value;
+        setValue(e.currentTarget.value);
+    }
 
     return (
         <> <input
-            onChange={(e) => {
-                e.currentTarget.value;
-                setValue(e.currentTarget.value)
-            }}
+            onChange={ onInputChangeValue }
             value={value}/> ---- {value}
         </>
     )
 }
-export const GetValueOfUncontrolledInputByPressButton = () => {
-    const [value, setValue] = useState<>('')
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    // ref позволяет нам создать ссылку на элемент и связать ее с ним и в коде обращаться к ней по каррент...
     const inputRef = useRef<HTMLInputElement>(null)
-    const save = (e: React.MouseEvent<HTMLButtonElement>) => {
-        const el = inputRef.current as HTMLInputElement;
-        setValue(el.value)
+
+    const save = () => {
+        const inputEl = inputRef.current as HTMLInputElement
+        setValue(inputEl.value)
     }
 
-    return <>
-        (<input ref={inputRef}/>
-        <button
-            onClick={save}
-        >save
-        </button>
-        --- actual value: {value}
-        )
-    </>
+    return <><input ref={inputRef} id={'inputId'}/><button onClick={save}>sav</button>actual value: {value}</>
 }
 
 
